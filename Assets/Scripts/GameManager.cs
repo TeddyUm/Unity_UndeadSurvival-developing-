10986f3fb8b200e44e44bfa38950b427703016c0
TeddyUm <43351089+TeddyUm@users.noreply.github.com>
2023-02-25 16:22:43 -0800

    UndeadSurvuval First Commit
    Basic start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Vector2 inputVec;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxis("Horizontal"); 
+        inputVec.y = Input.GetAxis("Vertical");
+    }
+}
