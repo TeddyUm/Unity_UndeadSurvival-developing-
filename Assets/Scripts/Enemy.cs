a881e6cba9fdd658593fcf6f5e28cba9b1338c0b
TeddyUm <43351089+TeddyUm@users.noreply.github.com>
2023-02-25 21:51:20 -0800

    Player & Backgroud moving
    - Player move and animation
    - Background infinite move

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    Vector2 inputVec;
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +13,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inputVec.x = Input.GetAxis("Horizontal"); 
-        inputVec.y = Input.GetAxis("Vertical");
+        
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator anim;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,27 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        // position movement
+        Vector2 netxVec = inputVec * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + netxVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Run", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            sprite.flipX = inputVec.x < 0;
+        }
     }
 }
